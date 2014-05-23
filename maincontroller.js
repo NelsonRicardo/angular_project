36f3bd7e19d6fdad77502f7a1809c5d67eb9b144
Nelson Ricardo <nelson@nelsonricardo.com>
2014-05-23 00:09:04 -0400

    update/improve
    correcting duplicate items not showing up; using array of objects in
    place of array of strings

--- maincontroller.js
+++ maincontroller.js
@@ -3,7 +3,7 @@ app.controller("MainController", function($scope){
     $scope.newItem = null;
     $scope.addNew = function() {
         if ($scope.newItem != null && $scope.newItem != "") {
-            $scope.items.push($scope.newItem);
+            $scope.items.push({name:$scope.newItem});
             $scope.newItem = null;
         }
     }
